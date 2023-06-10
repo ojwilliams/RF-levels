6e92734a24db26699576b6f154a4e535365ea8fd
Oscar Williams <77359486+ojwilliams@users.noreply.github.com>
2023-06-10 20:05:08 +0100

    colourful !

--- v3/webserver.py
+++ v3/webserver.py
@@ -15,7 +15,29 @@ from apscheduler.schedulers.background import BackgroundScheduler
 
 app = Flask(__name__)
 
+RX1_url = 'http://10.0.101.109/data.xml'
+RX2_url = 'http://10.0.101.110/data.xml'
+RX3_url = 'http://10.0.101.111/data.xml'
 RX4_url = 'http://10.0.101.112/data.xml'
+
+frequency_rx1 = None
+mer_1_rx1 = None
+mer_2_rx1 = None
+mer_3_rx1 = None
+mer_4_rx1 = None
+
+frequency_rx2 = None
+mer_1_rx2 = None
+mer_2_rx2 = None
+mer_3_rx2 = None
+mer_4_rx2 = None
+
+frequency_rx3 = None
+mer_1_rx3 = None
+mer_2_rx3 = None
+mer_3_rx3 = None
+mer_4_rx3 = None
+
 frequency_rx4 = None
 mer_1_rx4 = None
 mer_2_rx4 = None
@@ -53,11 +75,41 @@ def MER_4(MER4):
 
 
 def retrieve_data():
-    global frequency_rx4, mer_1_rx4, mer_2_rx4, mer_3_rx4, mer_4_rx4
+    global frequency_rx1, mer_1_rx1, mer_2_rx1, mer_3_rx1, mer_4_rx1, frequency_rx2, mer_1_rx2, mer_2_rx2, mer_3_rx2, mer_4_rx2, frequency_rx3, mer_1_rx3, mer_2_rx3, mer_3_rx3, mer_4_rx3, frequency_rx4, mer_1_rx4, mer_2_rx4, mer_3_rx4, mer_4_rx4
+
+    with urlopen(RX1_url) as a:
+        tree = ET.parse(a)
+        rootrx1 = tree.getroot()
+
+    with urlopen(RX2_url) as b:
+        tree = ET.parse(b)
+        rootrx2 = tree.getroot()
+
+    with urlopen(RX3_url) as c:
+        tree = ET.parse(c)
+        rootrx3 = tree.getroot()
 
     with urlopen(RX4_url) as d:
         tree = ET.parse(d)
         rootrx4 = tree.getroot()
+
+    frequency_rx1 = frequency(rootrx1)
+    mer_1_rx1 = MER_1(rootrx1)
+    mer_2_rx1 = MER_2(rootrx1)
+    mer_3_rx1 = MER_3(rootrx1)
+    mer_4_rx1 = MER_4(rootrx1)
+
+    frequency_rx2 = frequency(rootrx2)
+    mer_1_rx2 = MER_1(rootrx2)
+    mer_2_rx2 = MER_2(rootrx2)
+    mer_3_rx2 = MER_3(rootrx2)
+    mer_4_rx2 = MER_4(rootrx2)
+
+    frequency_rx3 = frequency(rootrx3)
+    mer_1_rx3 = MER_1(rootrx3)
+    mer_2_rx3 = MER_2(rootrx3)
+    mer_3_rx3 = MER_3(rootrx3)
+    mer_4_rx3 = MER_4(rootrx3)
 
     frequency_rx4 = frequency(rootrx4)
     mer_1_rx4 = MER_1(rootrx4)
@@ -66,13 +118,29 @@ def retrieve_data():
     mer_4_rx4 = MER_4(rootrx4)
 
     # Print the values
+    print("Frequency: ", frequency_rx1)
+    print("MER 1: ", mer_1_rx1)
+    print("MER 2: ", mer_2_rx1)
+    print("MER 3: ", mer_3_rx1)
+    print("MER 4: ", mer_4_rx1)
+
+    print("Frequency: ", frequency_rx2)
+    print("MER 1: ", mer_1_rx2)
+    print("MER 2: ", mer_2_rx2)
+    print("MER 3: ", mer_3_rx2)
+    print("MER 4: ", mer_4_rx2)
+
+    print("Frequency: ", frequency_rx3)
+    print("MER 1: ", mer_1_rx3)
+    print("MER 2: ", mer_2_rx3)
+    print("MER 3: ", mer_3_rx3)
+    print("MER 4: ", mer_4_rx3)
+
     print("Frequency: ", frequency_rx4)
     print("MER 1: ", mer_1_rx4)
     print("MER 2: ", mer_2_rx4)
     print("MER 3: ", mer_3_rx4)
     print("MER 4: ", mer_4_rx4)
-
-
 
 @app.route('/')
 def index():
@@ -81,6 +149,24 @@ def index():
 
     # Create a dictionary with the data
     data = {
+        'frequency_rx1': frequency_rx1,
+        'mer_1_rx1': mer_1_rx1,
+        'mer_2_rx1': mer_2_rx1,
+        'mer_3_rx1': mer_3_rx1,
+        'mer_4_rx1': mer_4_rx1,
+
+        'frequency_rx2': frequency_rx2,
+        'mer_1_rx2': mer_1_rx2,
+        'mer_2_rx2': mer_2_rx2,
+        'mer_3_rx2': mer_3_rx2,
+        'mer_4_rx2': mer_4_rx2,
+
+        'frequency_rx3': frequency_rx3,
+        'mer_1_rx3': mer_1_rx3,
+        'mer_2_rx3': mer_2_rx3,
+        'mer_3_rx3': mer_3_rx3,
+        'mer_4_rx3': mer_4_rx3,
+
         'frequency_rx4': frequency_rx4,
         'mer_1_rx4': mer_1_rx4,
         'mer_2_rx4': mer_2_rx4,
@@ -95,6 +181,24 @@ def index():
 @app.route('/data')
 def get_data():
     data = {
+        'frequency_rx1': frequency_rx1,
+        'mer_1_rx1': mer_1_rx1,
+        'mer_2_rx1': mer_2_rx1,
+        'mer_3_rx1': mer_3_rx1,
+        'mer_4_rx1': mer_4_rx1,
+
+        'frequency_rx2': frequency_rx2,
+        'mer_1_rx2': mer_1_rx2,
+        'mer_2_rx2': mer_2_rx2,
+        'mer_3_rx2': mer_3_rx2,
+        'mer_4_rx2': mer_4_rx2,
+
+        'frequency_rx3': frequency_rx3,
+        'mer_1_rx3': mer_1_rx3,
+        'mer_2_rx3': mer_2_rx3,
+        'mer_3_rx3': mer_3_rx3,
+        'mer_4_rx3': mer_4_rx3,
+
         'frequency_rx4': frequency_rx4,
         'mer_1_rx4': mer_1_rx4,
         'mer_2_rx4': mer_2_rx4,
@@ -107,7 +211,7 @@ def get_data():
 if __name__ == '__main__':
     # Create and configure the scheduler
     scheduler = BackgroundScheduler()
-    scheduler.add_job(retrieve_data, 'interval', seconds=2.5, max_instances=1)
+    scheduler.add_job(retrieve_data, 'interval', seconds=3, max_instances=1)
     scheduler.start()
 
     app.run()
